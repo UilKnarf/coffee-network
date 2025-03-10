53f1503da289c31a7c06a9d1da16c914f893e75d
Frank Liu <155685046+frankliu10@users.noreply.github.com>
2025-03-10 15:33:48 -0700

    fixed callbackurl to heroku

--- config/passport.js
+++ config/passport.js
@@ -50,7 +50,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback",
+        callbackURL: "https://coffee-network-ff5156a09eb4.herokuapp.com/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
